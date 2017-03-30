36f6d2783133371963476011622b65c36a8338bf
Jakob Demler <jdemler@curry-software.com>
2017-03-30 12:47:37 +0200

    float based progress

--- src/utils/progress.rs
+++ src/utils/progress.rs
@@ -1,24 +1,33 @@
-#[derive(Ord, PartialOrd, Eq, PartialEq)]
-pub struct Progress(u16);
+use std::cmp::Ordering;
+
+#[derive(PartialEq, PartialOrd)]
+pub struct Progress(f32);
 
 impl Progress {
     pub fn done() -> Self {
-        Progress(10_000)
+        Progress(1f32)
     }
     
     pub fn from(a: u32, b: u32) -> Self {
         if b == 0 {
             Progress::done()
         } else {
-            Progress(((a * 10_000)/b) as u16)
+            Progress((a as f32/b as f32))
         }
     }
 
     pub fn project_amount(&self, until_now: u32) -> u32 {
-        until_now * (10000/self.0 as u32)
+        (until_now as f32 * 1f32/self.0) as u32
     }
 }
 
+impl Eq for Progress {}
+
+impl Ord for Progress {
+    fn cmp(&self, other: &Progress) -> Ordering {
+        self.partial_cmp(other).unwrap()
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -33,4 +42,14 @@ mod tests {
         assert_eq!(progress.project_amount(1), 10);
         assert_eq!(progress.project_amount(20), 200);
     }
+
+    #[test]
+    fn sub_percent(){
+        //0.001% done
+        let progress = Progress::from(1, 100_000);
+        assert_eq!(progress.project_amount(1), 100_000);
+        assert_eq!(progress.project_amount(10), 1_000_000);
+        assert_eq!(progress.project_amount(20), 2_000_000);
+    }
+    
 }
